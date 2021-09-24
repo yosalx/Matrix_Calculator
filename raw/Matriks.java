d1a0d583ef551f50c424179a40c34fce6d0c9bcb
irsyadazka <16520096@mahasiswa.itb.ac.id>
2021-09-24 15:18:58 +0700

    nambah dan modip

--- raw/Matriks.java
+++ raw/Matriks.java
@@ -2,54 +2,53 @@ package Tubes1.matriksgeming.raw;
 
 import java.util.*;
 
-public class Matriks {
-    public int RowMin = 0, RowMax = 100;
-    public int ColMin = 0, ColMax = 100;
-    public int Row, Col;
-    public double[][] Mtrx;
+class Matriks {
+    int RowMin = 0, RowMax = 100;
+    int ColMin = 0, ColMax = 100;
+    int Row, Col;
+    double[][] Mtrx;
     Scanner scanner = new Scanner(System.in);
 
-    public Matriks(int row, int col) {
+    void createMatriks(int row, int col) {
         this.Row = row;
         this.Col = col;
-        this.Mtrx = new double[row][col];
+        Mtrx = new double[row][col];
     }
 
-    public int getFirstIdxRow() {
+    int getFirstIdxRow() {
         return RowMin;
     }
 
-    public int getFirstIdxCol() {
+    int getFirstIdxCol() {
         return ColMin;
     }
 
-    public int getLastIdxRow() {
+    int getLastIdxRow() {
         return this.Row - 1;
     }
 
-    public int getLastIdxCol() {
+    int getLastIdxCol() {
         return this.Col - 1;
     }
 
-    public int countElmt() {
+    int countElmt() {
         return this.Row * this.Col;
     }
 
-    public double getElmt(int row, int col) {
+    double getElmt(int row, int col) {
         return this.Mtrx[row][col];
     }
 
-    public void rowSwap(int row1, int row2) {
-        double[] temp = Mtrx[row1];
-        Mtrx[row1] = Mtrx[row2];
-        Mtrx[row2] = temp;
+    void readMatrix() {
+        int i, j;
+        for (i = 0; i < this.Row; i++) {
+            for (j = 0; j < this.Col; j++) {
+                this.Mtrx[i][j] = scanner.nextDouble();
+            }
+        }
     }
 
-    public boolean isSquare() {
-        return (this.Row == this.Col);
-    }
-
-    public void writeMatrix() {
+    void writeMatrix() {
         int i, j;
         for (i = 0; i < this.Row; i++) {
             for (j = 0; j < this.Col; j++) {
@@ -65,30 +64,54 @@ public class Matriks {
         }
     }
 
-    public void multiplyRow(int row, double x) {
+    boolean isSquare() {
+        return (this.Row == this.Col);
+    }
+
+    boolean isIdentity(Matriks m) {
+        int i, j;
+        boolean ident = true;
+        for (i = 0; i < this.Row; i++) {
+            for (j = 0; j < this.Col; j++) {
+                if ((i == j) && (m.Mtrx[i][j] != 1)) {
+                    return false;
+                }
+                if ((i != j) && (m.Mtrx[i][j] != 0)) {
+                    return false;
+                }
+            }
+        }
+        return ident;
+    }
+    
+    void multiplyRow(int row, double x) {
         int j;
         for (j = 0; j < this.Col; j++) {
             Mtrx[row][j] = x*Mtrx[row][j];
         }
     }
 
-    public void multiplyCol(int col, double x) {
+    void multiplyCol(int col, double x) {
         int i;
         for (i = 0; i < this.Row; i++) {
             Mtrx[i][col] = x*Mtrx[i][col];
         }
     }
 
-    public void plusRow(int row1, int row2, double x) {
+    void plusRow(int row1, int row2, double x) {
         int j;
         for (j = 0; j < this.Col; j++) {
             Mtrx[row1][j] += x*Mtrx[row2][j];
         }
     }
 
-    public void minusRow(int row1, int row2, double x) {
+    void minusRow(int row1, int row2, double x) {
         plusRow(row1, row2, -x);
     }
 
-    
+    void rowSwap(int row1, int row2) {
+        double[] temp = Mtrx[row1];
+        Mtrx[row1] = Mtrx[row2];
+        Mtrx[row2] = temp;
+    }
 }
