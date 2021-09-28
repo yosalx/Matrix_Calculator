c8eef4a1c21b98a4fd09094267b81cb84ffceed3
yosalx <yoseph141002@gmail.com>
2021-09-28 22:13:07 +0700

    update driver and doubleregression

--- raw/Driver.java
+++ raw/Driver.java
@@ -5,13 +5,24 @@ import java.io.*;
 
 public class Driver {
     public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);        
         Matriks m = new Matriks();
-        m.createMatriks(3, 3);
-        //m.readMatrixfromFile();
-        //m.polynomRead();
-        m.writeMatrix();
-        System.out.println("\n");
-        m.adjoint_invers();
+        int n;
+        System.out.print("\nMasukkan banyak peubah: ");
+        n = input.nextInt();
+        int p;
+        System.out.print("Masukkan banyak data per peubah: ");
+        p = input.nextInt();
+        m.createMatriks(p, n+1);
+        m.readMatrix();
+        System.out.println("\nMasukkan nilai yang ingin ditafsir:");
+        double[] est =  new double[1];
+        est = new double[n];
+        int i;
+        for(i = 0; i < n; i++){
+            est[i] = input.nextDouble();
+        }
+        m.double_regression(m.Col-1, est );
         //m.findSPLwithInv();
         // m.polynomInterpolate(true);
     }
